9d5bf50fb7eed999dd2668899a48534aa654a0a7
David Calvert <davidcalvertfr@gmail.com>
2019-06-16 18:21:42 +0200

    Allow 'source' outside of FILES and disabled colors for lint-shellcheck.sh
    Signed-off-by: David Calvert <davidcalvertfr@gmail.com>

--- lint-shellcheck.sh
+++ lint-shellcheck.sh
@@ -5,4 +5,4 @@
 # Purpose: Find all shell scripts and test them with shellcheck
 ################################################################################
 
-find . -type f -name "*.sh" -exec shellcheck {} \;
+find . -type f -name "*.sh" -exec shellcheck -x --color=never {} \;
